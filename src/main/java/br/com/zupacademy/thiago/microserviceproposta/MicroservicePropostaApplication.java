e44a4a194e318d1315e1717b0df401d604b9c964
ThiagoZup <thiago.reis@zup.com.br>
2021-08-13 09:17:21 -0300

    setup

---
+++ src/main/java/br/com/zupacademy/thiago/microserviceproposta/MicroservicePropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.thiago.microserviceproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/thiago/microserviceproposta/MicroservicePropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.thiago.microserviceproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
